df36081abfb5b74d23bc10d93c8f27cfb3b8f034
erush4 <ethan.t.rushforth@gmail.com>
2025-01-24 22:31:24 -0700

    debug KingMoveCalculator

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -10,9 +10,13 @@ public class KingMoveCalculator extends ChessMoveCalculator {
     public void calculateMove(ChessBoard board, ChessPosition start) {
         for(int i = -1; i <= 1; i +=2) {
             for(int j = -1; j <= 1; j +=2) {
-                ChessPosition next = new ChessPosition(start.getRow() + i, start.getColumn() + j);
+                int row = start.getRow() + i;
+                int column = start.getColumn() + j;
+                boolean inBounds = row > 0 && row <= 8 && column > 0 && column <= 8;
+                ChessPosition next = new ChessPosition(row, column);
                 ChessPiece nextSpace = board.getPiece(next);
-                if (nextSpace == null || nextSpace.getTeamColor() != board.getPiece(next).getTeamColor()) {
+                boolean validSquare = nextSpace == null || nextSpace.getTeamColor() != board.getPiece(next).getTeamColor();
+                if (inBounds && validSquare) {
                     ChessMove newMove = new ChessMove(start, next, null);
                     addMove(newMove);
                 }
