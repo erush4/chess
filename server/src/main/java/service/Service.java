2d982fd8171c11b7d2af00fde8b7a382a9f6e2ce
erush4 <ethan.t.rushforth@gmail.com>
2025-03-03 15:58:47 -0700

    implement CreateGame
    all unit tests passing

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,5 +1,6 @@
 package service;
 
+import chess.ChessGame;
 import dataAccess.DataAccessException;
 import dataAccess.DataAccess;
 import model.*;
@@ -16,6 +17,19 @@ public class Service {
         dataAccess.createAuth(newAuthData);
         return authToken;
     }
+
+    private AuthData verifyAuthData(String authToken) throws ResponseException {
+        try {
+            AuthData authData = dataAccess.getAuth(authToken);
+            if (authData == null) {
+                throw new ResponseException(401, "unauthorized");
+            }
+            return authData;
+        } catch (DataAccessException e) {
+            throw new ResponseException(500, "could not get data");
+        }
+    }
+
     public Service(DataAccess dataAccess) {
         this.dataAccess = dataAccess;
     }
@@ -70,10 +84,7 @@ public class Service {
 
     public void logout(String authToken) throws ResponseException {
         try{
-            AuthData authData= dataAccess.getAuth(authToken);
-            if (authData == null){
-                throw new ResponseException(401, "unauthorized");
-            }
+            AuthData authData= verifyAuthData(authToken);
             dataAccess.deleteAuth(authData);
         } catch (DataAccessException e) {
             throw new ResponseException(500, "could not get data");
@@ -82,12 +93,24 @@ public class Service {
 
     public ListGamesResponse listGames(String authToken) throws ResponseException{
         try{
-            AuthData authData = dataAccess.getAuth(authToken);
-            if (authData == null){
-                throw new ResponseException(401, "unauthorized");
-            }
+            verifyAuthData(authToken);
             return new ListGamesResponse(dataAccess.listGames());
         } catch (DataAccessException e) {
+            throw new ResponseException(500, "could not get data");
+        }
+    }
+
+    public Integer createGame(String authToken, String gameName) throws ResponseException {
+        if (gameName == null){
+            throw new ResponseException(400, "bad request");
+        }
+        try{
+            verifyAuthData(authToken);
+            int gameID = UUID.randomUUID().hashCode();
+            GameData game = new GameData(gameID, null, null, gameName, new ChessGame());
+            dataAccess.addGame(game);
+            return gameID;
+        } catch (DataAccessException e){
             throw new ResponseException(500, "could not get data");
         }
     }
