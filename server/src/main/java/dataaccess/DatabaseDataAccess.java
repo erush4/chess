64fb04191039bb74f0d6d83a7a67ab58929fb22a
erush4 <ethan.t.rushforth@gmail.com>
2025-03-12 16:13:30 -0600

    implement configureDatabase and updateDatabase

--- server/src/main/java/dataaccess/DatabaseDataAccess.java
+++ server/src/main/java/dataaccess/DatabaseDataAccess.java
@@ -4,11 +4,15 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class DatabaseDataAccess implements DataAccess {
-    public DatabaseDataAccess() {
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
+public class DatabaseDataAccess implements DataAccess {
+    public DatabaseDataAccess() throws DataAccessException {
+        configureDatabase();
     }
 
     @Override
@@ -59,5 +63,62 @@ public class DatabaseDataAccess implements DataAccess {
     @Override
     public void updateGame(GameData newGameData) throws DataAccessException {
 
+    }
+
+    private int updateDatabase(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case String p -> ps.setString(i + 1, p);
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> throw new DataAccessException("Bad parameter type: " + param.getClass().getName());
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private final String[] createStatements = {"""
+            CREATE TABLE IF NOT EXISTS users (
+               username VARCHAR(32),
+               passhash VARCHAR(64),
+               email VARCHAR(64),
+               PRIMARY KEY (username)
+            )
+            CREATE TABLE IF NOT EXISTS games (
+                gameid VARCHAR(64),
+                whiteusername VARCHAR(32),
+                blackusername VARCHAR(32),
+                gamename VARCHAR(32),
+                gamejson TEXT
+                )
+            CREATE TABLE IF NOT EXISTS authdata (
+                authtoken VARCHAR(64)
+                username VARCHAR(32)
+            )"""};
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Could not configure database: " + ex.getMessage());
+        }
     }
 }
