f187fd3afc5ee0c2692735de382e4c687add189a
erush4 <ethan.t.rushforth@gmail.com>
2025-03-24 23:58:10 -0600

    create ServerFacade
    no tests written yet

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,94 @@
+package server;
+
+import com.google.gson.Gson;
+import model.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    private static void writeBody(Object requestObject, String authtoken, HttpURLConnection http) throws IOException {
+        if (requestObject != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            http.addRequestProperty("authorization", authtoken);
+            String reqData = new Gson().toJson(requestObject);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest gameRequest, String authtoken) throws ResponseException {
+        return this.request("POST", "/game", gameRequest, authtoken, CreateGameResponse.class);
+    }
+
+    public void joinGame(JoinGameRequest joinRequest, String authtoken) throws ResponseException {
+        this.request("PUT", "/game", joinRequest, authtoken, null);
+    }
+
+    public ListGamesResponse listGames(String authtoken) throws ResponseException {
+        return this.request("GET", "/game", null, authtoken, ListGamesResponse.class);
+    }
+
+    public LoginResponse login(LoginRequest loginRequest) throws ResponseException {
+        return this.request("POST", "/session", loginRequest, null, LoginResponse.class);
+    }
+
+    public RegisterResponse register(RegisterRequest registerRequest) throws ResponseException {
+        return this.request("POST", "/session", registerRequest, null, RegisterResponse.class);
+    }
+
+    public void logout(String authtoken) throws ResponseException {
+        this.request("DELETE", "/session", null, authtoken, null);
+    }
+
+    public void clear() throws ResponseException {
+        this.request("DELETE", "/db", null, null, null);
+    }
+
+    private <T> T request(String method, String path, Object requestObject, String authtoken, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(requestObject, authtoken, http);
+            http.connect();
+
+            int status = http.getResponseCode();
+            if (status != 200) {
+                try (InputStream respError = http.getErrorStream()) {
+                    if (respError != null) {
+                        throw ResponseException.fromStream(respError);
+                    }
+                }
+            }
+
+            T response = null;
+            if (http.getContentLength() < 0) {
+                try (InputStream respBody = http.getInputStream()) {
+                    InputStreamReader reader = new InputStreamReader(respBody);
+                    if (responseClass != null) {
+                        response = new Gson().fromJson(reader, responseClass);
+                    }
+                }
+            }
+            return response;
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- shared/src/main/java/model/ResponseException.java
+++ shared/src/main/java/model/ResponseException.java
@@ -1,8 +1,10 @@
 package model;
 
 import com.google.gson.Gson;
-
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
+import java.util.HashMap;
 
 public class ResponseException extends Exception {
     final private int statusCode;
@@ -15,6 +17,12 @@ public class ResponseException extends Exception {
 
     public String toJson() {
         return new Gson().toJson(Map.of("message", getMessage()));
+    }
+    public static ResponseException fromStream(InputStream stream) {
+        var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);
+        var status = ((Double)map.get("status")).intValue();
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
     }
 
     public int getStatusCode() {
