de03fccf242a4b2535409e05c4a8811b29181b21
erush4 <ethan.t.rushforth@gmail.com>
2025-04-09 18:52:34 -0600

    implement help

--- client/src/main/java/ui/GameplayRepl.java
+++ client/src/main/java/ui/GameplayRepl.java
@@ -15,14 +15,7 @@ public class GameplayRepl extends ReplTemplate {
     }
 
     private String makeMove(String[] params) {
-        return  SET_TEXT_COLOR_BLUE + "move <START ROW> <START COLUMN> <END ROW> <END COLUMN>"
-                    + SET_TEXT_COLOR_LIGHT_GREY + " - makes the selected chess move, if valid\n"
-                + SET_TEXT_COLOR_BLUE + "highlight <ROW> <COLUMN>" + SET_TEXT_COLOR_LIGHT_GREY
-                    + " - highlights valid moves for the selected piece\n"
-                + SET_TEXT_COLOR_BLUE + "redraw" + SET_TEXT_COLOR_LIGHT_GREY + " - redraws the board\n"
-                + SET_TEXT_COLOR_BLUE + "resign" + SET_TEXT_COLOR_LIGHT_GREY + " - resign from the game\n"
-                + SET_TEXT_COLOR_BLUE + "leave" + SET_TEXT_COLOR_LIGHT_GREY + " - leave the game\n"
-                + SET_TEXT_COLOR_BLUE + "help" + SET_TEXT_COLOR_LIGHT_GREY + " - show this page again";
+        return  "";
     }
 
     private String resign() {
@@ -35,13 +28,20 @@ public class GameplayRepl extends ReplTemplate {
 
     @Override
     String help() {
-        return "";
+        return SET_TEXT_COLOR_BLUE + "move <START ROW> <START COLUMN> <END ROW> <END COLUMN>"
+                + SET_TEXT_COLOR_LIGHT_GREY + " - makes the selected chess move, if valid\n"
+                + SET_TEXT_COLOR_BLUE + "highlight <ROW> <COLUMN>" + SET_TEXT_COLOR_LIGHT_GREY
+                + " - highlights valid moves for the selected piece\n"
+                + SET_TEXT_COLOR_BLUE + "redraw" + SET_TEXT_COLOR_LIGHT_GREY + " - redraws the board\n"
+                + SET_TEXT_COLOR_BLUE + "resign" + SET_TEXT_COLOR_LIGHT_GREY + " - resign from the game\n"
+                + SET_TEXT_COLOR_BLUE + "leave" + SET_TEXT_COLOR_LIGHT_GREY + " - leave the game\n"
+                + SET_TEXT_COLOR_BLUE + "help" + SET_TEXT_COLOR_LIGHT_GREY + " - show this page again";
     }
 
     @Override
     String functions(String command, String[] params) throws ResponseException {
         return switch (command) {
-            case "leave" -> RESET_COLOR + "Leaving the game...";
+            case "leave" -> RESET_COLOR + "You have left the game.";
             case "resign" -> resign();
             case "highlight" -> highlightMoves(params);
             case "redraw" -> redraw();
